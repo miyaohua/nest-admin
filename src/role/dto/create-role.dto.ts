141110511e721d6af16e8383e8e66ab46ca0e1b3
miyaohua <2542571191@qq.com>
2024-04-23 22:26:42 +0800

    feat:add role

--- src/role/dto/create-role.dto.ts
+++ src/role/dto/create-role.dto.ts
@@ -1,5 +1,8 @@
 import { IsNotEmpty } from 'class-validator'
+import { IsUniqueField } from '../../rules/unique-field.rule'
 export class CreateRoleDto {
+
     @IsNotEmpty({ message: '请输入角色名称' })
+    @IsUniqueField('role', { message: '该用户角色已被添加' })
     name: string
 }
